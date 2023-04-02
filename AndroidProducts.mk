f4f98da842afb61acf0757c817fabad516fb1df2
xddeath69 <85794863+xddeath69@users.noreply.github.com>
2023-04-02 20:19:29 -0300

    Update AndroidProducts.mk
    add suport  cherish

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,13 @@ COMMON_LUNCH_CHOICES := \
     lineage_santoni-userdebug \
     lineage_santoni-user \
     lineage_santoni-eng
+
+
+# CherishOS
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/cherish_santoni.mk
+
+COMMON_LUNCH_CHOICES := \
+    cherish_santoni-userdebug \
+    cherish_santoni-user \
+    cherish_santoni-eng
