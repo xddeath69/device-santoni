32ee7844b16af6346fb0b47d8c4d813567bc6261
xddeath69 <85794863+xddeath69@users.noreply.github.com>
2023-09-11 12:44:25 -0300

    Update AndroidProducts.mk
    add xtended

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,12 @@ COMMON_LUNCH_CHOICES := \
    havoc_santoni-userdebug \
     havoc_santoni-user \
    havoc_santoni-eng
+
+# xt (xtended)
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/xtended_santoni.mk
+
+COMMON_LUNCH_CHOICES := \
+    xtended_santoni-userdebug \
+    xtended_santoni-user \
+    xtended_santoni-eng
